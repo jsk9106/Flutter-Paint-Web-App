46a8391095eee03ec50911c723b36943e1f8c9bd
Noah <noah@noteasy.kr>
2022-05-25 10:09:44 +0900

    git test03

--- lib/screens/game/game_screen.dart
+++ lib/screens/game/game_screen.dart
@@ -13,7 +13,7 @@ import '../components/palette.dart';
 import 'components/user_list.dart';
 import 'show_screen.dart';
 
-// git test02
+// git test03
 class GameScreen extends StatelessWidget {
   GameScreen({Key? key, required this.room}) : super(key: key);
 
